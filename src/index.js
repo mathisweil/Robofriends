9dcb8060b4f953a6297367ead99bf6f1190c002c
mathisweil <mathis.weil@outlook.com>
2024-02-27 13:53:30 +0000

    Robofriends app

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { StrictMode } from 'react';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
+import './index.css';
+import App from './containers/App';
+import 'tachyons';
+// import registerServiceWorker from './registerServiceWorker';
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
